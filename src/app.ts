4ac630f30c69de7fa653f669a1679b018301dd35
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2023-09-24 17:22:51 +0400

    feat: create user routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -7,65 +7,6 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-interface IUser {
-  name: string;
-  about: string;
-  avatar: string;
-}
-
-interface ICard {
-  name: string;
-  link: string;
-  owner:mongoose.Types.ObjectId;
-  likes: mongoose.Types.ObjectId[];
-  createdAt: Date;
-}
-const userSchema = new mongoose.Schema<IUser>({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  about: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 200,
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-});
-
-const cardSchema = new mongoose.Schema<ICard>({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  link: {
-    type: String,
-    required: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    default: [],
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
