d79137cf4fa5ec881d29e2436d43afd3332f6778
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2023-09-26 22:49:42 +0400

    fix: correct validation errors_v1

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use('/users', userRouter);
 
 app.use('/cards', cardRouter);
 
-app.use((req, res, next) => {
+app.use('*', (req, res, next) => {
   res.status(NOT_FOUND);
   next(new Error('Страница не найдена'));
 });
